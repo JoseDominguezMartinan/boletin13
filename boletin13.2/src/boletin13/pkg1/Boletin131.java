b923134b50c88a93a17e125f925076e5c5b68d40
jose <jose@jose-PC>
2017-12-16 17:30:17 +0100

    boletin13terminados

--- boletin13.2/src/boletin13/pkg1/Boletin131.java
+++ boletin13.2/src/boletin13/pkg1/Boletin131.java
@@ -7,7 +7,6 @@ package boletin13.pkg1;
 
 import java.util.Scanner;
 
-
 /**
  *
  * @author jose
@@ -19,25 +18,24 @@ public class Boletin131 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-      
+
         // creamos un obxeto de tipo Scanner para que o usuario inserte a temperatura a convertir 
-        
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         // creamos un obxeto de tipo ConversorTemperaturas
-        ConversorTemperaturas conversor=new ConversorTemperaturas();
+        ConversorTemperaturas conversor = new ConversorTemperaturas();
         // pedimos por teclado a temperatura a convertir
         System.out.println("Inserte a temperatura da que se quere facer a conversión");
-        float grados=sc.nextFloat();
+        float grados = sc.nextFloat();
         // capturamos a exceppcion no caso de convertir a grados fhareint
         try {
             conversor.centigradosAFhareint(grados);
-            System.out.println(conversor.getFahreint()+"ºFhareint");
+            System.out.println(conversor.getFahreint() + "ºFhareint");
         } catch (TemperaturaErradaExcepcion ex1) {
             System.out.println("Non se poden convertir a fahreint  temperaturas menores a 80ºc");
         }
         // no caso de grados reamur xa o fixemos na clase ConversorTemperaturas, polo que solo facenmos a chamada
         conversor.centigradosAReamur(grados);
-        
+
     }
-    
+
 }
--- boletin13.2/src/boletin13/pkg1/ConversorTemperaturas.java
+++ boletin13.2/src/boletin13/pkg1/ConversorTemperaturas.java
@@ -16,59 +16,51 @@ public class ConversorTemperaturas {
     public final float temperatura = 80;
     float fahreint;
     float reamur;
-    boolean i=true;
-    Scanner sc=new Scanner(System.in);
-    
+    boolean i = true;
+    Scanner sc = new Scanner(System.in);
+
     /**
-     * metodo que fara a conversion de grados centigrados a fahreint
-     * a excepcion neste caso será tratada no metodo main 
+     * metodo que fara a conversion de grados centigrados a fahreint a excepcion
+     * neste caso será tratada no metodo main
+     *
      * @param centigrados
      * @return
-     * @throws TemperaturaErradaExcepcion 
+     * @throws TemperaturaErradaExcepcion
      */
-
     public float centigradosAFhareint(float centigrados) throws TemperaturaErradaExcepcion {
         if (centigrados < temperatura) {
-            
+
             throw new TemperaturaErradaExcepcion();
-            
+
         } else {
             return fahreint = 9f / 5f * centigrados + 32.4f;
         }
     }
-    
-    /**
-     * metodo que realiza a conversion de centigrados a reamus
-     * neste caso capturarase aqui a excepcion
-     * @param centigrados
-     * @return 
-     */
 
+    /**
+     * metodo que realiza a conversion de centigrados a reamus neste caso
+     * capturarase aqui a excepcion
+     *
+     * @param centigrados
+     * @return
+     */
     public float centigradosAReamur(float centigrados) {
-          while( i=true){  
-       
-            
-            if (centigrados < temperatura){
+        while (i = true) {
+
+            if (centigrados < temperatura) {
                 System.out.println("numero non valido,volva a insertar o numero ");
-                centigrados=sc.nextFloat();
-               
+                centigrados = sc.nextFloat();
+
+            } else {
+
+                reamur = 4.0f / 5.0f * centigrados;
+                System.out.println(reamur + "grados Reamur");
+                i = false;
+                break;
             }
-                
-                
-               
-            else
-          
-            {
-      
-           reamur = 4.0f / 5.0f * centigrados;
-            System.out.println(reamur+"grados Reamur");
-            i=false;
-            break;
-            }
-        } 
-        
-       
-     return reamur;     
+        }
+
+        return reamur;
     }
 
     public float getFahreint() {
@@ -78,5 +70,5 @@ public class ConversorTemperaturas {
     public float getReamur() {
         return reamur;
     }
-    
+
 }
